b0c0b937e8415d3494cb15c66fb208e704233ce6
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-10-02 17:11:13 -0500

    Added some post processing, fixed camera angles

---
+++ Assets/TopDownCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float followSpeed;
+
+	void LateUpdate ()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, followSpeed * Time.deltaTime);
+	}
+}
